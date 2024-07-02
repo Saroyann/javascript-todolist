d3b8b755b5d16437ee6c81612ee851b23fbe8cc1
Yiim <ilyimyan@gmail.com>
2024-07-02 14:26:23 +0800

    initial commit

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 const inputBox = document.getElementById('input-box');
 const listContainer = document.getElementById('list-container');
+
+const addTask = ()=> {
+    if(inputBox === '') {
+        alert('inputan todolist tidak boleh kosong kosong');
+    } else {
+        let li = document.createElement('li');
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+    }
+}
