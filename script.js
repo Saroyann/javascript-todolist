dc1d5e0614290efbdc182e0a8a348b3cfe2eb5d7
Yiim <ilyimyan@gmail.com>
2024-07-02 14:33:46 +0800

    initial commit

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const addTask = ()=> {
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
         let span = document.createElement('span');
-        
+        span.innerHTML = '\u00d7';
     }
     inputBox.value = '';
 }
